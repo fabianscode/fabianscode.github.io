bffe6d82a59500bf3d1cfdc33aa81365e9b8d5ef
fabiancodes <fabiancodes@gmai.como>
2020-09-24 23:52:53 +0200

    app update

---
+++ android/schachklub-bingen/app/src/main/java/com/example/schachklubbingen/App.kt
@@ -0,0 +1,8 @@
+package com.example.schachklubbingen
+
+class App {
+    // static instance for all time access to MainActivity.kt
+    companion object {
+        lateinit var mainActivity: MainActivity
+    }
+}
