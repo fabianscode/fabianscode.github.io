9c07d1f91dcdf6fc576c326f40d83287ade8ffff
42Tobias42 <54958846+42Tobias42@users.noreply.github.com>
2020-10-28 17:23:15 +0100

    news-section-function

--- js/news.js
+++ js/news.js
@@ -1,10 +1,18 @@
-
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
-  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+  if (document.body.scrollTop > getHeight() || document.documentElement.scrollTop > getHeight()) {
     document.getElementById("navbar-years").style.top = "0";
   } else {
     document.getElementById("navbar-years").style.top = "-56px";
   }
+}
+
+function getHeight(){
+	  divElement = document.querySelector(".height-function"); 
+   
+      elemHeight = divElement.clientHeight; 
+
+      return elemHeight;
 }
